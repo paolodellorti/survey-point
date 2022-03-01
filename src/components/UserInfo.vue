86c57ebdf64490ebe6cae1b6269732977b48d830
Paolo <paolodellorti@gmail.com>
2022-03-01 17:15:00 +0000

    added id

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h3>Paolo Dell'Orti</h3>
+  <div class="main">
+    <div class="title">
+      <h3>Paolo Dell'Orti</h3>
+      <h6># 457384V</h6>
+    </div>
     <el-avatar icon="el-icon-user-solid" :size="50"></el-avatar>  
   </div>
 </template>
@@ -12,14 +15,23 @@ export default {
 </script>
 
 <style scoped>
-  div {
+  .main {
     display: flex;
     justify-content: center;
     align-items: center;
     border-bottom: 3px solid #000;
     padding: 10px 0;
   }
-  h3 {
+  .title>h3 {
+    margin-bottom: 0;
     margin-right: 50px;
+  }
+  .title>h6 {
+    margin-top: 0;
+    text-align: left;
+    color: rgb(160, 160, 160);
+  }
+  h3 {
+
   }
 </style>
